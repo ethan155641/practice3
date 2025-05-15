8c822e6d1a71ed2c9e240873ad165a8d33536af1
ethan155641 <130410233+ethan155641@users.noreply.github.com>
2025-05-15 13:36:30 -0400

    Update Program.cs

--- newproject/Program.cs
+++ newproject/Program.cs
@@ -1,101 +1,107 @@
-﻿using System;
+using System;
+using System.Globalization;
 
 class FinanceCalc
 {
     static void Main()
     {
-        //menu 1-4
         while (true)
         {
-            Console.WriteLine("select an option by selecting a number");
-            Console.WriteLine("1. calculate simple intrest");
-            Console.WriteLine("2. Calculate Compound Intrest");
-            Console.WriteLine("3. Calculate monthly payment");
+            Console.WriteLine("\nSelect an option by entering a number:");
+            Console.WriteLine("1. Calculate Simple Interest");
+            Console.WriteLine("2. Calculate Compound Interest");
+            Console.WriteLine("3. Calculate Monthly Payment");
             Console.WriteLine("4. Exit");
 
             string input = Console.ReadLine();
 
-            //call functions
-            if (input == "1")
+            switch (input)
             {
-                SimpleIntrest();
-            }
-            else if (input == "2")
-            {
-                CompoundIntrest();
-
-            }
-            else if (input == "3")
-            {
-                Payment();
-            }
-            else if (input == "4")
-            {
-                Console.WriteLine("Exiting......");
-                break; //kill loop
-            }
-            else
-            {
-                Console.WriteLine("stop trying to break my program");
+                case "1":
+                    SimpleInterest();
+                    break;
+                case "2":
+                    CompoundInterest();
+                    break;
+                case "3":
+                    Payment();
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please enter 1-4.");
+                    break;
             }
         }
     }
-    static void SimpleIntrest()
+
+    static double ReadDouble(string prompt)
     {
-
-        Console.Write("Enter Principal");
-        double Principal = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Entar rate as a %");
-        double Rate = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("enter # of years");
-        int time = Convert.ToInt32(Console.ReadLine());
-
-        double intrest = Principal * Rate * time / 100;
-        Console.WriteLine($"you will pay ={intrest:F2}");
-
+        double value;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out value))
+                return value;
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
     }
 
-    static void CompoundIntrest()
+    static int ReadInt(string prompt)
     {
+        int value;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out value))
+                return value;
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
 
-        Console.Write("Enter Principal");
-        double Principal = Convert.ToDouble(Console.ReadLine());
+    static void SimpleInterest()
+    {
+        double principal = ReadDouble("Enter Principal: ");
+        double rate = ReadDouble("Enter Rate as a %: ");
+        int time = ReadInt("Enter Number of Years: ");
 
-        Console.WriteLine("Entar rate as a %");
-        double Rate = Convert.ToDouble(Console.ReadLine());
+        double interest = principal * rate * time / 100;
+        Console.WriteLine($"You will pay: {interest:F2}");
+    }
 
-        Console.WriteLine("enter # of years");
-        int time = Convert.ToInt32(Console.ReadLine());
+    static void CompoundInterest()
+    {
+        double principal = ReadDouble("Enter Principal: ");
+        double rate = ReadDouble("Enter Rate as a %: ");
+        int time = ReadInt("Enter Number of Years: ");
+        int number = ReadInt("Enter Number of Times Interest is Compounded per Year: ");
 
-        Console.WriteLine(" enter # of times intres is compouded");
-        int number = Convert.ToInt32(Console.ReadLine());
+        double amount = principal * Math.Pow(1 + rate / (100 * number), number * time);
+        double interest = amount - principal;
 
-        double amount = Principal * Math.Pow(1 + Rate / (100 * number), number * time);
-        double interest = amount - Principal;
-
-        Console.WriteLine($"Compound Interest = {interest:F2}");
+        Console.WriteLine($"Compound Interest: {interest:F2}");
     }
 
     static void Payment()
     {
+        double principal = ReadDouble("Enter Principal: ");
+        double rate = ReadDouble("Enter Rate as a %: ");
+        int time = ReadInt("Enter Number of Years: ");
 
-        Console.Write("Enter Principal");
-        double Principal = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Entar rate as a %");
-        double Rate = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("enter # of years");
-        int time = Convert.ToInt32(Console.ReadLine());
-
-        double monthlyRate = Rate / (12 * 100); // convert to monthly decimal
+        double monthlyRate = rate / (12 * 100);
         int months = time * 12;
 
-        double emi = (Principal * monthlyRate * Math.Pow(1 + monthlyRate, months)) /
-                     (Math.Pow(1 + monthlyRate, months) - 1);
-
-        Console.WriteLine($"Monthly Payment (EMI) = {emi:F2}");
+        if (monthlyRate == 0)
+        {
+            double emi = principal / months;
+            Console.WriteLine($"Monthly Payment (EMI): {emi:F2}");
+        }
+        else
+        {
+            double emi = (principal * monthlyRate * Math.Pow(1 + monthlyRate, months)) /
+                         (Math.Pow(1 + monthlyRate, months) - 1);
+            Console.WriteLine($"Monthly Payment (EMI): {emi:F2}");
+        }
     }
 }
